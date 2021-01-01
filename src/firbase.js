90694cd8c6f952ead16870c851d4a1eef707734f
Ahmed Habib <ahmedboboooooko@gmail.com>
2021-01-01 19:27:40 +0200

    Initial commit

---
+++ src/firbase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC0QfE0r4uuuwGnkxT_MQPql7lKfc2-ukQ",
+    authDomain: "clone-22440.firebaseapp.com",
+    projectId: "clone-22440",
+    storageBucket: "clone-22440.appspot.com",
+    messagingSenderId: "632246382125",
+    appId: "1:632246382125:web:a77db412f94c0d449992a4"
+  };
---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyCCuGT10AMPmi-SCmGVsLOwNJ-CFiKDT18";
+
+export default API_KEY;
